4ecc3a0c7a3ae79b3728882b67ae281f38ff856a
bsametarman@outlook.com <bsametarman@outlook.com>
2022-04-10 16:45:38 +0300

    IR Remote support added

--- policeFlash.ino
+++ policeFlash.ino
@@ -1,55 +1,32 @@
-int key = 0;
-int closeKey = 0;
+#include <IRremote.h>
+
+//- Global declerations -//
+int animNum = 0;
+int closeKey = 1;
+int RECV_PIN = A3;
+
+IRrecv irrecv(RECV_PIN);
+decode_results results;
 
 // ---------------------------------------------
 
-void setup() {
-  for(int i = 2; i <= 11; i++)
-  {
-    pinMode(i, OUTPUT);
-  }
-  pinMode(A0, INPUT);
-  pinMode(A2, INPUT);
-
-}
-
-// ---------------------------------------------
-
-void animChangeControl()
-{
-   if(digitalRead(A0) == true)
-   {
-    key++;
-    if(key == 9)
-    {
-      key = 0;
-    }
-   }
-}
-
-// ---------------------------------------------
-
-void onOffControl()
-{
-   if(digitalRead(A2) == true)
-   {
-    closeKey++;
-    if(closeKey == 2)
-    {
-      closeKey = 0;
-    }
-   }
-}
-
-// ---------------------------------------------
-
-void turnOff()
-{
-  for(int i = 2; i <= 11; i++)
-  {
-    digitalWrite(i, LOW);
-  }
-}
+long BUTTON_ONE = 1452207117;
+long BUTTON_TWO = 378749304;
+long BUTTON_THREE = 3197347044;
+long BUTTON_FOUR = 841470775;
+long BUTTON_FIVE = 286230856;
+long BUTTON_SIX = 2335683509;
+long BUTTON_SEVEN = 727798456;
+long BUTTON_EIGHT = 2065345736;
+long BUTTON_NINE = 4092985532;
+long BUTTON_ZERO = 117062114;
+long BUTTON_CLOSE = 2586515089;
+long BUTTON_TURN_SIGN = 1643148896;
+long BUTTON_HD = 2298376329;
+long BUTTON_RED = 2319031341;
+long BUTTON_GREEN = 1969982189;
+long BUTTON_YELLOW = 1672214345;
+long BUTTON_BLUE = 3865988176;
 
 // ---------------------------------------------
 
@@ -61,7 +38,7 @@ void anim1()
     digitalWrite(3, HIGH);
     digitalWrite(4, HIGH);
     digitalWrite(5, HIGH);
-  
+    
     delay(50);
 
  
@@ -70,7 +47,7 @@ void anim1()
     digitalWrite(4, LOW);
     digitalWrite(5, LOW);
     
-    delay(50);  
+    delay(50);
   }
 
   for(int i = 0; i < 2; i++)
@@ -79,15 +56,14 @@ void anim1()
     digitalWrite(9, HIGH);
     digitalWrite(10, HIGH);
     digitalWrite(11, HIGH);
-  
+    
     delay(50);
-
      
     
     digitalWrite(8, LOW);
     digitalWrite(9, LOW);
     digitalWrite(10, LOW);
-    digitalWrite(11, LOW);
+    digitalWrite(11, LOW);   
     
     delay(50);  
   }
@@ -111,8 +87,6 @@ void anim2()
       digitalWrite(6, HIGH);
       digitalWrite(7, HIGH);
       delay(10);
-
-       
       
       digitalWrite(6, LOW);
       digitalWrite(7, LOW);
@@ -124,7 +98,7 @@ void anim2()
     digitalWrite(4, LOW);
     digitalWrite(5, LOW);
     
-    delay(50);  
+    delay(50);
   }
 
   for(int i = 0; i < 2; i++)
@@ -133,7 +107,7 @@ void anim2()
     digitalWrite(9, HIGH);
     digitalWrite(10, HIGH);
     digitalWrite(11, HIGH);
-  
+    
     delay(50);
     
     for(int i = 0; i < 2; i++)
@@ -141,8 +115,6 @@ void anim2()
       digitalWrite(6, HIGH);
       digitalWrite(7, HIGH);
       delay(10);
-
-       
       
       digitalWrite(6, LOW);
       digitalWrite(7, LOW);
@@ -154,7 +126,7 @@ void anim2()
     digitalWrite(10, LOW);
     digitalWrite(11, LOW);
     
-    delay(50);  
+    delay(50);
   }
 }
 
@@ -168,8 +140,6 @@ void anim3()
     digitalWrite(9, HIGH);
     digitalWrite(10, HIGH);
     digitalWrite(11, HIGH);
-
-     
     
     digitalWrite(2, HIGH);
     digitalWrite(3, HIGH);
@@ -177,22 +147,19 @@ void anim3()
     digitalWrite(5, HIGH);
   
     delay(50);
-
    
     
     digitalWrite(8, LOW);
     digitalWrite(9, LOW);
     digitalWrite(10, LOW);
     digitalWrite(11, LOW);
-
-     
     
     digitalWrite(2, LOW);
     digitalWrite(3, LOW);
     digitalWrite(4, LOW);
     digitalWrite(5, LOW);
     
-    delay(50);  
+    delay(50);
   }
 }
 
@@ -206,8 +173,6 @@ void anim4()
     digitalWrite(9, HIGH);
     digitalWrite(10, HIGH);
     digitalWrite(11, HIGH);
-
-     
     
     digitalWrite(2, HIGH);
     digitalWrite(3, HIGH);
@@ -215,22 +180,18 @@ void anim4()
     digitalWrite(5, HIGH);
     
     delay(50);
-
-     
     
     digitalWrite(8, LOW);
     digitalWrite(9, LOW);
     digitalWrite(10, LOW);
     digitalWrite(11, LOW);
-
-     
     
     digitalWrite(2, LOW);
     digitalWrite(3, LOW);
     digitalWrite(4, LOW);
     digitalWrite(5, LOW);
     
-    delay(50); 
+    delay(50);
   }
 
   for(int i = 0; i < 7; i++)
@@ -239,8 +200,6 @@ void anim4()
     digitalWrite(7, HIGH);
     
     delay(50);
-
-     
     
     digitalWrite(6, LOW);
     digitalWrite(7, LOW);
@@ -264,7 +223,6 @@ void anim5()
   digitalWrite(7, HIGH);
   
   delay(100);
-
  
   
   digitalWrite(2, HIGH);
@@ -286,10 +244,8 @@ void anim5()
   digitalWrite(3, LOW);
   digitalWrite(4, LOW);
   digitalWrite(5, LOW);
-
   delay(100);
 
- 
 
   digitalWrite(2, HIGH);
   digitalWrite(3, HIGH);
@@ -310,7 +266,6 @@ void anim5()
   
   delay(100);
 
- 
 
   digitalWrite(8, LOW);
   digitalWrite(9, LOW);
@@ -324,7 +279,6 @@ void anim5()
   digitalWrite(3, LOW);
   digitalWrite(4, LOW);
   digitalWrite(5, LOW);
-
   delay(100);
 }
 
@@ -334,7 +288,7 @@ void anim6()
 {
   for(int i = 2; i <= 11; i++)
   {
-    digitalWrite(i, HIGH); 
+    digitalWrite(i, HIGH);
   }
   
   delay(75);
@@ -415,7 +369,6 @@ void anim8()
   digitalWrite(11, LOW);
 
   delay(50);
-
   digitalWrite(3, LOW);
   digitalWrite(10, LOW);
 
@@ -440,42 +393,93 @@ void anim8()
 
 // ---------------------------------------------
 
-void loop() {
-
- animChangeControl();
-
- onOffControl();
-
-if (closeKey == 1)
+void turnOff()
 {
-   switch (key){
-    case 0:
-      anim7();
-      break;
-    case 1:
-      anim1();
-      break;
-    case 2:
-      anim2();
-      break;
-    case 3:
-      anim3();
-      break;
-    case 4:
-      anim4();
-      break;
-    case 5:
-      anim5();
-      break;
-    case 6:
-      anim6();
-      break;
-    case 7:
-      anim8();
-      break;
-   }
+  for(int i = 2; i <= 11; i++)
+  {
+    digitalWrite(i, LOW);
+  }
 }
-else
-  kapali();
+
+// ---------------------------------------------
+
+void setup() {
+  for(int i = 2; i <= 11; i++)
+  {
+    pinMode(i, OUTPUT);
+  }
+  pinMode(A0, INPUT);
+  pinMode(A2, INPUT);
+
+  irrecv.enableIRIn();
+}
+
+// ---------------------------------------------
+
+void loop() {
   
+  if (irrecv.decode(&results))
+  {
+     irrecv.resume();
+     if(results.value == BUTTON_ONE){
+        animNum = 0;
+     }
+     if(results.value == BUTTON_TWO){
+        animNum = 1;
+     }
+     if(results.value == BUTTON_THREE){
+        animNum = 2;
+     }
+     if(results.value == BUTTON_FOUR){
+        animNum = 3;
+     }
+     if(results.value == BUTTON_FIVE){
+        animNum = 4;
+     }
+     if(results.value == BUTTON_SIX){
+        animNum = 5;
+     }
+     if(results.value == BUTTON_SEVEN){
+        animNum = 6;
+     }
+     if(results.value == BUTTON_EIGHT){
+        animNum = 7;
+     }
+     if(results.value == BUTTON_NINE){
+        animNum = 8;
+     }
+     if(results.value == BUTTON_CLOSE){
+        animNum = 9;
+     }     
+  } 
+
+  switch (animNum){
+      case 0:
+        anim7();
+        break;
+      case 1:
+        anim1();
+        break;
+      case 2:
+        anim2();
+        break;
+      case 3:
+        anim3();
+        break;
+      case 4:
+        anim4();
+        break;
+      case 5:
+        anim5();
+        break;
+      case 6:
+        anim6();
+        break;
+      case 7:
+        anim8();
+        break;
+      case 9:
+        turnOff();
+        break;
+     }
 }
